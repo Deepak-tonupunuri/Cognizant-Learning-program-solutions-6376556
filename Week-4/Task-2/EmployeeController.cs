d1ff60a9e4cd6a4ff26be195c52dd7dea2a58742
Deepak Tonupunuri <deepaktonupunuri@gmail.com>
2025-07-13 10:10:20 +0530

    Add files via upload

---
+++ Week-4/Task-2/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/emp")] // changed from [controller] to 'emp'
+    public class EmployeeController : ControllerBase
+
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = new[]
+            {
+                new { Id = 1, Name = "John", Department = "Developer" },
+                new { Id = 2, Name = "Vick", Department = "Service Manager" },
+            };
+            return Ok(employees);
+        }
+    }
+}
