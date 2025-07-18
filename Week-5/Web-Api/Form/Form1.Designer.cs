35d0c7af95d2b9bbdd3a7ea70a1f82ae6e350186
Deepak Tonupunuri <deepaktonupunuri@gmail.com>
2025-07-18 15:12:47 +0530

    Add files via upload

---
+++ Week-5/Web-Api/Form/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace KafkaChatFormApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox lstChat;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lstChat = new System.Windows.Forms.ListBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstChat
+            // 
+            this.lstChat.FormattingEnabled = true;
+            this.lstChat.ItemHeight = 15;
+            this.lstChat.Location = new System.Drawing.Point(12, 12);
+            this.lstChat.Name = "lstChat";
+            this.lstChat.Size = new System.Drawing.Size(460, 214);
+            this.lstChat.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 240);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(370, 23);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(388, 239);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(84, 25);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 281);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lstChat);
+            this.Name = "Form1";
+            this.Text = "Kafka Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
