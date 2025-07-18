b3737d94ac5616a9efdd418cf760520a98bbcc19
Deepak Tonupunuri <deepaktonupunuri@gmail.com>
2025-07-18 15:21:10 +0530

    Add files via upload

---
+++ Week-5/MicroServices/Controllers/SecureDataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureDataController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("This is a secret message only for authorized users!   ie, Deepak Tonupunuri");
+        }
+    }
+}
